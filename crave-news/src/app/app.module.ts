bc0ee717080d751a5b5ff8f5afa6be4447ff4dc6
amitvchaudhary <amit.chaudhary140@gmail.com>
2017-05-28 19:12:29 +0530

    Added Material design icons

--- crave-news/src/app/app.module.ts
+++ crave-news/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
